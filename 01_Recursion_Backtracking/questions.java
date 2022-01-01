c1e837515f35ee9e0e9297211a6b569cce225b3e
Rajat <rajat08pahuja@gmail.com>
2022-01-01 20:59:29 +0530

    Update

--- 01_Recursion_Backtracking/questions.java
+++ 01_Recursion_Backtracking/questions.java
@@ -449,6 +449,101 @@ public class questions {
         printTargetSumSubsets(arr, idx + 1, set, sos, tar);
     }
 
+    // Floodfill
+    public static void floodfill(int[][] maze, int sr, int sc, String asf) {
+        if (sr < 0 || sr >= maze.length || sc < 0 || sc >= maze[0].length || maze[sr][sc] == 1) {
+            return;
+        }
+
+        if (sr == maze.length - 1 && sc == maze[0].length - 1) {
+            System.out.println(asf);
+            return;
+        }
+
+        maze[sr][sc] = 1;
+        floodfill(maze, sr - 1, sc, asf + 't');
+        floodfill(maze, sr, sc - 1, asf + 'l');
+        floodfill(maze, sr + 1, sc, asf + 'd');
+        floodfill(maze, sr, sc + 1, asf + 'r');
+        maze[sr][sc] = 0;
+    }
+
+    // N Queens
+    public static boolean isSafe(int[][] chess, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 1) {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 1) {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+            if (chess[i][j] == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printNQueens(int[][] chess, String qsf, int row) {
+        if (row == chess.length) {
+            System.out.println(qsf + ".");
+            return;
+        }
+
+        for (int i = 0; i < chess.length; i++) {
+            if (isSafe(chess, row, i)) {
+                chess[row][i] = 1;
+                printNQueens(chess, qsf + row + "-" + i + ", ", row + 1);
+                chess[row][i] = 0;
+            }
+        }
+    }
+
+    // Knights Tour
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+        if (r < 0 || c < 0 || r >= chess.length || c >= chess.length || chess[r][c] != 0) {
+            return;
+        }
+
+        if (upcomingMove == chess.length * chess.length) {
+            chess[r][c] = upcomingMove;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
+        }
+
+        chess[r][c] = upcomingMove;
+
+        printKnightsTour(chess, r - 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c - 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 2, c - 1, upcomingMove + 1);
+
+        chess[r][c] = 0;
+    }
+
+    public static void displayBoard(int[][] chess) {
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
     }
